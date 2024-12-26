063aa2c81d02ae43a0538960b6b798efff119538
Jon Barker <jbarker@Jons-Laptop.local>
2024-12-26 11:03:37 -0700

    2015 day 10 part 2

--- 2015/10/10.py
+++ 2015/10/10.py
@@ -1,7 +1,7 @@
 # input = 111221 # 312211 (test)
-input = 1113222113 # (real input)
+orig_input = str(1113222113) # (real input)
 
-input = str(input)
+# PART I
 
 def split_runs(input):
     seq = input[0]
@@ -16,11 +16,23 @@ def split_runs(input):
         split.append(seq)
     return split
 
+input = orig_input
+
 for _ in range(40):
     split = split_runs(input)
-    input = ''
-    for s in split:
-        input += str(len(s))
-        input += s[0]
+    input = [str(len(s)) + str(s[0]) for s in split]
+    input = ''.join(input)
+
+print(len(input))
+
+# PART II
+
+input = orig_input
+
+for i in range(50):
+    print(i)
+    split = split_runs(input)
+    input = [str(len(s)) + str(s[0]) for s in split]
+    input = ''.join(input)
 
 print(len(input))
