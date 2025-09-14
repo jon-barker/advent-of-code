721bcb39a9290ef38888a95a948dcad30aef4222
Jon Barker <jbarker@Jons-Laptop.local>
2025-09-14 17:29:02 -0600

    2015 day 12 part 1

---
+++ 2015/12/12.py
@@ -0,0 +1,16 @@
+with open("input.txt", "r") as f:
+    input = f.read()
+
+parsed = ""
+new = False
+for i, c in enumerate(input):
+    if c in '-0123456789':
+        if new:
+            parsed += " "
+            new = False
+        parsed += c
+    else:
+        new = True   
+
+num_list = [int(p) for p in parsed.split(" ")[1:]]
+print(sum(num_list))
