bb1674e54c5b1bd15ad4118b874d5680213be744
Jon Barker <jbarker@Jons-Laptop.local>
2025-12-09 19:58:18 -0700

    2025 day 9 - finished part ii

--- 2025/9/9.py
+++ 2025/9/9.py
@@ -1,6 +1,7 @@
 with open("input.txt", "r") as f:
     coords = f.readlines()
-coords = [c.strip('\n').split(',') for c in coords]
+coords = [reversed(c.strip('\n').split(',')) for c in coords]
+# coords are (col, row) pairs with zero indexing
 coords = [[int(c) for c in point] for point in coords]
 
 # PART I
@@ -17,18 +18,20 @@ print(max_area)
 
 import numpy as np
 
-max0 = 0
-max1 = 1
+maxc = 0
+maxr = 1
 for c in coords:
-    if c[0] > max0:
-        max0 = c[0]
-    if c[1] > max1:
-        max1 = c[1]
+    if c[0] > maxc:
+        maxc = c[0]
+    if c[1] > maxr:
+        maxr = c[1]
 
 print("Marking corners...")
-grid = np.zeros((max1 + 1, max0 + 1), dtype=np.bool)
+grid = np.zeros((maxc + 1, maxr + 1), dtype=np.bool)
 for c in coords:
-    grid[c[1], c[0]] = 1
+    grid[c[0], c[1]] = 1
+print(grid.sum(0).max(), grid.sum(1).max())
+print(grid)
 
 # draw outline
 # there are only ever two corners per row or column of the grid
@@ -41,77 +44,77 @@ for a in coords:
         if (a[1] == b[1]):
             y = a[1]
         if x is not None:
-            for y in range(min(a[1], b[1]), max(a[1], b[1])):
-                grid[y, x] = 1
+            for y in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
+                grid[x, y] = 1
         if y is not None:
-            for x in range(min(a[0], b[0]), max(a[0], b[0])):
-                grid[y, x] = 1
+            for x in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
+                grid[x, y] = 1
+print(grid)
 
-# flood fill
-print("Flood filling...")
-p = [coords[0][1], coords[0][0]]
-u = grid[p[0]-1, p[1]]
-r = grid[p[0], p[1]+1]
-l = grid[p[0], p[1]-1]
-d = grid[p[0]+1, p[1]]
-start = [0, 0]
-if u:
-    start[0] = p[0]-1
-if r:
-    start[1] = p[1]+1
-if l:
-    start[1] = p[1]-1
-if d:
-    start[0] = p[0]+1
+# get indices of nonzero values in each row
+bounds = [grid[r, :].nonzero()[0] for r in range(grid.shape[0])]
+bounds = [[min(b), max(b)] if len(b) > 0 else [] for b in bounds]
 
+# fill in the outline
+def in_bounds(bounds, p):
+    x, y = p
+    row = bounds[y]
+    if not row: return False
+    return row[0] <= x <= row[1]
 
-from collections import deque
-def flood_fill_bfs(grid, start):
-    rows, cols = grid.shape
-    sr, sc = start
-    if not (0 <= sr < rows and 0 <= sc < cols):
-        return grid
-    if grid[sr, sc]:
-        return grid
-
-    visited = np.zeros_like(grid, dtype=bool)
-    q = deque()
-    q.append((sr, sc))
-    visited[sr, sc] = True
-
-    while q:
-        print(len(q))
-        r, c = q.popleft()
-        if grid[r, c] == 0:
-            grid[r, c] = 1
-        for nr, nc in ((r-1,c), (r+1,c), (r,c-1), (r,c+1)):
-            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr, nc] and grid[nr, nc] == 0:
-                visited[nr, nc] = True
-                q.append((nr, nc))
-
-    return grid
-
-print(flood_fill_bfs(grid, start))
+print("Filling outline...")
+for i, b in enumerate(bounds):
+    if len(b) > 0:
+        grid[i, b[0]:b[1]+1] = 1
+print(grid)
 
 # test areas
 print("Finding largest area...")
+
+# given tl = (x0, y0), br = (x1, y1)
+def check_rect_edges(grid, tl, br):
+    x0, y0 = tl
+    x1, y1 = br
+    # width/height
+    if x0 > x1 or y0 > y1:
+        return False
+
+    # left col
+    if not grid[x0, y0:y1+1].all():
+        return False
+    # right col (skip if same as left)
+    if y1 != y0:
+        if not grid[x1, y0:y1+1].all():
+            return False
+    # top row (exclude corners already checked)
+    if y1 - y0 > 1:
+        if not grid[x0+1:x1, y0].all():
+            return False
+    # bottom row (skip if same row)
+    if x1 != x0 and y1 - y0 > 1:
+        if not grid[x0+1:x1, y1].all():
+            return False
+
+    return True
+
 max_area = 0
 counter = 0
 for i, a in enumerate(coords):
     for b in coords[i+1:]:
         if counter % 100 == 0:
             print(f"{counter} of {(len(coords) ** 2) // 2 }")
-        if a[0]**2 + a[1]**2 > b[0]**2 + b[1]**2:
-            tl = b
-            br = a
-        else:
-            tl = a
-            br = b
-        area = (br[0] - tl[0] + 1) * (br[1] - tl[1] + 1)
-        if (area > max_area):
-            if grid[tl[1]:br[1]+1, tl[0]:br[0]+1].all():
-                max_area = area
+        if (a[0] != b[0]):
+            if (a[1] != b[1]):
+                x0 = min(a[0], b[0])
+                x1 = max(a[0], b[0])
+                y0 = min(a[1], b[1])
+                y1 = max(a[1], b[1])
+                area = (x1 - x0 + 1) * (y1 - y0 + 1)
+                if (area > max_area):
+                    if check_rect_edges(grid, (x0, y0), (x1, y1)):
+                        # if edges are all valid do a final check on the whole rectangle
+                        if grid[x0:x1+1, y0:y1+1].all():
+                            max_area = area
         counter += 1
 
 print(max_area)
-        
